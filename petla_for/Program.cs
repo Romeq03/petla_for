d640629c88bc403b89a85ddc3db3ece38f1c5900
Romeq03 <romekbartek18@gmail.com>
2023-12-09 16:29:13 +0100

    first commit

---
+++ petla_for/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Function Calculator: y = sqrt(3^2) * x");
+
+        for (int x = 0; x <= 10; x++)
+        {
+            double y = Math.Sqrt(3 * 3) * x;
+            Console.WriteLine($"For x = {x}, y = {y}");
+        }
+    }
+}
